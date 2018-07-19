69ccc99d9fb73fe1d1dd23cf10223bfc35d6ce9b
Ricardo Otero <oterosantos@gmail.com>
2018-07-19 18:42:13 +0100

    Rubocop fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
-require "bundler/setup"
-require "zoho_hub"
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'simplecov'
+SimpleCov.start do
+  add_group 'Records', 'zoho_hub/records'
+end
+
+require 'zoho_hub'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
--- spec/zoho_hub_spec.rb
+++ spec/zoho_hub_spec.rb
@@ -1,9 +1,7 @@
-RSpec.describe ZohoHub do
-  it "has a version number" do
-    expect(ZohoHub::VERSION).not_to be nil
-  end
+# frozen_string_literal: true
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe ZohoHub do
+  it 'has a version number' do
+    expect(ZohoHub::VERSION).not_to be nil
   end
 end
