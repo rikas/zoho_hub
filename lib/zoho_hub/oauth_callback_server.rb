2bf8d349b486d7eeb6c0917d38e26fdd98e136f2
rikas <oterosantos@gmail.com>
2018-11-27 11:53:30 +0000

    Create oauth_callback_server.rb

---
+++ lib/zoho_hub/oauth_callback_server.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+
+module ZohoHub
+  class OauthCallbackServer < Sinatra::Base
+    enable :logging
+
+    CALLBACK_PATH = 'oauth2callback'
+
+    before do
+      content_type :json
+    end
+
+    get "/#{CALLBACK_PATH}" do
+      grant_token = params[:code]
+
+      # This will trigger a post request to get both the token and the refresh token
+      @variables = ZohoHub::Auth.get_token(grant_token)
+
+      puts "Variables: #{@variables.inspect}"
+
+      erb :variables
+    end
+  end
+end
