e48427c329a273ed3468bd5416f7a9d454e8734a
rikas <oterosantos@gmail.com>
2018-12-10 23:28:12 +0000

    Remove access type

--- lib/zoho_hub/configuration.rb
+++ lib/zoho_hub/configuration.rb
@@ -2,7 +2,7 @@
 
 module ZohoHub
   class Configuration
-    attr_accessor :client_id, :secret, :redirect_uri, :api_domain, :access_type
+    attr_accessor :client_id, :secret, :redirect_uri, :api_domain
     attr_writer :debug
 
     DEFAULT_API_DOMAIN = 'https://accounts.zoho.eu'
@@ -12,7 +12,6 @@ module ZohoHub
       @secret = ''
       @redirect_uri = ''
       @api_domain = DEFAULT_API_DOMAIN
-      @access_type = Auth::DEFAULT_ACCESS_TYPE
     end
 
     def debug?
