3bddcafd495aec420d2b393b62aa0d19c7ce31c0
rikas <oterosantos@gmail.com>
2018-12-12 11:54:01 +0000

    Add prompt=consent to auth url when access type is offline

--- lib/zoho_hub/auth.rb
+++ lib/zoho_hub/auth.rb
@@ -12,6 +12,7 @@ module ZohoHub
     extend Forwardable
 
     TOKEN_PATH = '/oauth/v2/token'
+    REVOKE_TOKEN_PATH = '/oauth/v2/token/revoke'
     AUTH_PATH = '/oauth/v2/auth'
 
     DEFAULT_SCOPES = %w[
@@ -31,14 +32,6 @@ module ZohoHub
       @scopes = scopes
     end
 
-    def token_full_uri
-      Addressable::URI.join(api_domain, TOKEN_PATH)
-    end
-
-    def auth_full_uri
-      Addressable::URI.join(api_domain, AUTH_PATH)
-    end
-
     def self.auth_url(access_type: DEFAULT_ACCESS_TYPE, scopes: DEFAULT_SCOPES)
       new(access_type: access_type, scopes: scopes).auth_url
     end
@@ -46,7 +39,7 @@ module ZohoHub
     def auth_url
       uri = auth_full_uri
 
-      uri.query_values = {
+      query = {
         client_id: client_id,
         scope: @scopes.join(','),
         access_type: @access_type,
@@ -54,21 +47,27 @@ module ZohoHub
         response_type: 'code'
       }
 
+      # The consent page must be presented otherwise we don't get the refresh token back.
+      query[:prompt] = 'consent' if @access_type == DEFAULT_ACCESS_TYPE
+
+      uri.query_values = query
+
       Addressable::URI.unencode(uri.to_s)
     end
 
-    def token_url(grant_token)
-      uri = token_full_uri
+    def auth_full_uri
+      Addressable::URI.join(api_domain, AUTH_PATH)
+    end
 
-      uri.query_values = {
-        client_id: client_id,
-        client_secret: secret,
-        code: grant_token,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      }
+    def self.refresh_token(refresh_token)
+      new.refresh_token(refresh_token)
+    end
 
-      Addressable::URI.unencode(uri.to_s)
+    def refresh_token(refresh_token)
+      result = Faraday.post(refresh_url(refresh_token))
+
+      json = parse(result.body)
+      json.merge(refresh_token: refresh_token)
     end
 
     def refresh_url(refresh_token)
@@ -84,15 +83,8 @@ module ZohoHub
       Addressable::URI.unencode(uri.to_s)
     end
 
-    def self.refresh_token(refresh_token)
-      new.refresh_token(refresh_token)
-    end
-
-    def refresh_token(refresh_token)
-      result = Faraday.post(refresh_url(refresh_token))
-
-      json = parse(result.body)
-      json.merge(refresh_token: refresh_token)
+    def token_full_uri
+      Addressable::URI.join(api_domain, TOKEN_PATH)
     end
 
     def revoke_refresh_token(refresh_token)
@@ -116,6 +108,22 @@ module ZohoHub
 
       parse(result.body)
     end
+
+    def token_url(grant_token)
+      uri = token_full_uri
+
+      uri.query_values = {
+        client_id: client_id,
+        client_secret: secret,
+        code: grant_token,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      }
+
+      Addressable::URI.unencode(uri.to_s)
+    end
+
+    private
 
     def parse(body)
       MultiJson.load(body, symbolize_keys: true)
