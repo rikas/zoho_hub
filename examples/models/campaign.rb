fb030230df0e220ec5bfd495326f023afd5da2a7
rikas <oterosantos@gmail.com>
2019-06-05 20:18:29 +0100

    Simple campaign example

--- examples/models/campaign.rb
+++ examples/models/campaign.rb
@@ -3,12 +3,11 @@
 require 'zoho_hub/records/base_record'
 
 module ZohoHub
+  # Simple manual Zoho record where we just want to get the name. Something like:
+  # #<ZohoHub::Campaign:0x00007fce2cc22458 @id="78265000003433063", @name="Smith & Williamson">
   class Campaign < BaseRecord
     attributes :id, :name
 
-    def initialize(params)
-      @id = params[:id]
-      @name = params[:Campaign_Name]
-    end
+    attribute_translation name: :Campaign_Name
   end
 end
