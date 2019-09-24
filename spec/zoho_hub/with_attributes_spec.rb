aed38c551ebc70c2d6a36d72eef7cb99ac130f66
rikas <oterosantos@gmail.com>
2019-09-24 11:48:29 +0100

    Remove leaky class

--- spec/zoho_hub/with_attributes_spec.rb
+++ spec/zoho_hub/with_attributes_spec.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe ZohoHub::WithAttributes do
-  class TestClass
-    include ZohoHub::WithAttributes
+  let(:test_class) do
+    Class.new do
+      include ZohoHub::WithAttributes
 
-    attributes :one, :two, :three
+      attributes :one, :two, :three
+    end
   end
 
   describe '.attributes' do
     context 'when attributes are passed' do
       it 'adds the attributes to the list of attr_accessors' do
-        test = TestClass.new
+        test = test_class.new
         test.one = 1
         test.two = 2
 
@@ -21,14 +23,14 @@ RSpec.describe ZohoHub::WithAttributes do
 
     context 'when attributes are not passed' do
       it 'returns the list of attributes' do
-        expect(TestClass.attributes).to eq(%i[one two three])
+        expect(test_class.attributes).to eq(%i[one two three])
       end
     end
   end
 
   describe '#attributes' do
     it 'returns the class attributes' do
-      test = TestClass.new
+      test = test_class.new
       expect(test.attributes).to eq(%i[one two three])
     end
   end
@@ -36,13 +38,13 @@ RSpec.describe ZohoHub::WithAttributes do
   describe '#assign_attributes' do
     context 'when the argument is not a hash' do
       it 'raises an ArgumentError' do
-        test = TestClass.new
+        test = test_class.new
         expect { test.assign_attributes('one: 1, two: 2') }.to raise_exception(ArgumentError)
       end
     end
 
     it 'assign the object attributes' do
-      test = TestClass.new
+      test = test_class.new
       test.assign_attributes(one: 1, two: 2)
       expect(test.one).to eq(1)
       expect(test.two).to eq(2)
