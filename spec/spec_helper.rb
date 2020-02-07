3c6734e931b35a394eefbfc4e979fec40754af45
rikas <oterosantos@gmail.com>
2020-02-07 11:29:45 +0000

    Add first vcr config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,20 @@ require 'bundler/setup'
 require 'simplecov'
 SimpleCov.start
 
+require 'dotenv'
+Dotenv.load
+
 require 'zoho_hub'
+
+ZohoHub.configure do |config|
+  config.client_id    = ENV['ZOHO_CLIENT_ID']
+  config.secret       = ENV['ZOHO_SECRET']
+  config.redirect_uri = ENV['ZOHO_REDIRECT_URI']
+end
+
+token_params = ZohoHub::Auth.refresh_token(ENV['ZOHO_REFRESH_TOKEN'])
+puts token_params
+ZohoHub.setup_connection(token_params)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -15,5 +28,15 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+end
+
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<TOKEN>') do
+    token_params[:access_token]
   end
 end
---
+++ spec/zoho_hub/with_connection_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe ZohoHub::WithConnection do
+  class TestClass
+    include ZohoHub::WithConnection
+  end
+
+  describe '.get' do
+    it 'fires a get request with ZohoHub::Connection' do
+      VCR.use_cassette('modules_get') do
+        result = TestClass.get('/settings/modules')
+      end
+    end
+  end
+end
